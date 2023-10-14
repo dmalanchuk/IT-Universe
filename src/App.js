903954b867f9673471646fec10c6f80aff94802f
Даніїл Марисик <danamarisik@MacBook-Air-Dana.local>
2023-10-14 17:05:15 +0300

    added router system

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import Slidenav from "./components/Slidenav";
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import Index from "./pages/Index.page";
+import Login from "./pages/Login.page"
+import Regist from "./pages/Regist.page";
+
+const router = createBrowserRouter([
+  {
+    path: '/index',
+    element: <Index />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/reg',
+    element: <Regist />
+  }
+])
 
 function App() {
   return (
     <div className="App">
-      <Slidenav />
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/pages/Regist.page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Regist = () => {
+  return (
+    <div>Regist.page</div>
+  )
+}
+
+export default Regist
